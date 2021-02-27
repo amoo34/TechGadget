020985fcb2e9e75386be40e791b33562b94190cf
Abdul Moiz <a.moizfaisal123@gmail.com>
2021-02-27 12:48:29 +0500

    update

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
      <Route path="/Productnewpage/:id" component={Productnewpage}/>
       <Route path="/Admin" component={Ahome}/>
       <Route path="/Shop" component={myProduct}/>
-      <Route path="/Cart" component={Cart}/>
+      <Route path="/Cart" component={Cart}/
       <Route path="/Contact" component={Contact}/>
       <Route path="/SignIn" component={signIn}/>
       <Route path="/SignUp" component={Signup}/>
