aefe2343162acd9995da56b8c7ba3686bed1332d
Abdul Moiz <a.moizfaisal123@gmail.com>
2021-02-27 14:48:31 +0500

    ss

--- src/App.js
+++ src/App.js
@@ -24,7 +24,8 @@ function App() {
   const Product =    storeProducts;
   return (
     <React.Fragment>
-    
+      <p>Hello World</p>
+      <h1>Helicopter shot</h1>
       <div className="content">
       <Switch>
       <Route path="/"  exact component={Home}/> 
@@ -34,7 +35,7 @@ function App() {
       <Route path="/AdminOrder" component={Aorder}/>
      <Route path="/Productnewpage/:id" component={Productnewpage}/>
       <Route path="/Admin" component={Ahome}/>
-      <Route path="/Shop" component={myProduct}/>
+      <Route path="/Shop" component={myProduct}/> 
       <Route path="/Cart" component={Cart}/>
       <Route path="/Contact" component={Contact}/>
       <Route path="/SignIn" component={signIn}/>
